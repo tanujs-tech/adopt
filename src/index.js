0240c8869749dc7ac6c9be2348a636d4378c811e
Sumit Rajput <sumitr@techracers.com>
2018-02-17 15:22:38 -0700

    Integrated redux-sagas

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import './index.css'
 import App from './app'
 import registerServiceWorker from './registerServiceWorker'
 import reducer from './reducers'
+import createSagaMiddleware from 'redux-saga'
+import { initSagas } from './sagas'
 
-const store = createStore(reducer)
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware),
+)
+
+sagaMiddleware.run(initSagas)
 
 ReactDOM.render(
   <Provider store={store}>
