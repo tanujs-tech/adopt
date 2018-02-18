0b47cf8aabf2dd83df3b914204dee346e3c637d3
Sumit Rajput <sumitr@techracers.com>
2018-02-18 04:01:28 -0700

    Fixed issue in reducers

--- src/reducers/pet.js
+++ src/reducers/pet.js
@@ -18,43 +18,43 @@ const initialState = {
 const pet = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_PET_REQUEST:
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         isFetching: true,
       });
     case CREATE_PET_REQUEST_SUCCESS:
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         isFetching: false,
       });
     case CREATE_PET_REQUEST_FAILURE:
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         isFetching: false,
         error: action.error
       })
 
     case CREATE_PET_ADOPTION_REQUEST:
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         isFetching: true,
       });
     case CREATE_PET_ADOPTION_REQUEST_SUCCESS:
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         isFetching: false,
       });
     case CREATE_PET_ADOPTION_REQUEST_FAILURE:
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         isFetching: false,
         error: action.error
       })
 
     case ADOPT_PET_REQUEST:
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         isFetching: true,
       });
     case ADOPT_PET_REQUEST_SUCCESS:
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         isFetching: false,
       });
     case ADOPT_PET_REQUEST_FAILURE:
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         isFetching: false,
         error: action.error
       })
